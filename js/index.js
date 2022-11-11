34a53832f7f41ca23df99409014be246c8c56e58
Regina Castagnoli <111323056+ReginaCastagnoli@users.noreply.github.com>
2022-11-11 10:01:41 -0300

    prompt y alert

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ class Producto {
     }
 }
 
-//agrego los objetos con arreglos
+//agrego los objetos con arreglosgitt ad
 
 const velaAlmendra = new Producto ( "vela", " frasco almendra de 290 cc.", 550, " esta disponible" );
 const velaHogarenia = new Producto ( "vela", " frasco anchoero de 100 cc", 330, " esta disponible" );
@@ -22,3 +22,26 @@ const arcoirisGuirnalda = new Producto ( "arcoiris", "Ideal para decorar cuarto 
 const arcoirisColgante = new Producto ( "arcoiris", "Arociris de 30 cm.", 450, " esta disponible" );
 const arcoirisLlavero = new Producto ( "llavero", "Llavero en forma de Arcoiris nordico", 280, " esta disponible" );
 const cactusLlavero = new Producto ( "llavero", "Llavero en forma de cactus nordico.", 280, " esta disponible" );
+
+
+
+//entrada con prompt
+
+const productoElegido = prompt(`Ingrese el numero del producto elegido
+ 1. Vela Almendra
+ 2. Vela Hogareña
+ 3. Vela Clasica
+ 4. Vela Eco
+ 5. Atrapasueño Clasico
+ 6. Atrapasueño Arbol de la vida
+ 7. Atrapasueño con Frase
+ 8. Atrapasueño Luna
+ 9. Arcoiris Guirnalda
+ 10. Arcoiris Colgante
+ 11. Arcoiris Llavero
+ 12. Cactus Llavero
+ 13. Ninguno
+`);
+
+//salida con alert 
+alert("Usted eligio el producto nº" + productoElegido);
